f010065f71bc8dcb78b5c9d61c05631c1f587342
alexmwaura <alexmwaura43@gmail.com>
2022-02-24 12:14:59 +0300

    fix eslint & product-list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  products!: Product[];
 
-  constructor() { }
+  constructor(private ProductService: ProductService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.listProducts();
   }
 
+  listProducts() {
+    this.ProductService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
